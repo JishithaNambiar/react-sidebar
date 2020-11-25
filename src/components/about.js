638b63188e343dc50ca69c3b77ca50168b1f1d91
JishithaNambiar <73138851+JishithaNambiar@users.noreply.github.com>
2020-11-25 10:06:55 -0500

    Update about.js
    Added space around h6 and the button

--- src/components/about.js
+++ src/components/about.js
@@ -1,28 +1,27 @@
 import React, { Component } from "react";
-import Button from './button.js'
+import Button from "./button.js";
 import Header from "./header";
-import edele from './fixed logo example.png';
-import img6278 from './IMG_6278.jpg';
-import heat from './9082575_heart (2).png';
-import mary from './FB_IMG_1601691929258.jpg';
-import face from './Untitled-2-01_2_1_1_1_1_1_2_1.png';
-import cc from './Screenshot_20201002-231437_Instagram.jpg';
-import smoosh from './Screenshot_20201003-231223_Instagram.jpg';
-import credit from './crs - 1.png';
-import crown from './CROWNBLACK2.png';
-import amelia from './Screenshot_20201008-002034_Drive.jpg';
-import toolate from './Too Late - Cover - Sized.png';
+import edele from "./fixed logo example.png";
+import img6278 from "./IMG_6278.jpg";
+import heat from "./9082575_heart (2).png";
+import mary from "./FB_IMG_1601691929258.jpg";
+import face from "./Untitled-2-01_2_1_1_1_1_1_2_1.png";
+import cc from "./Screenshot_20201002-231437_Instagram.jpg";
+import smoosh from "./Screenshot_20201003-231223_Instagram.jpg";
+import credit from "./crs - 1.png";
+import crown from "./CROWNBLACK2.png";
+import amelia from "./Screenshot_20201008-002034_Drive.jpg";
+import toolate from "./Too Late - Cover - Sized.png";
 
 const About = () => {
   return (
     <div>
       <Header />
       <div className="card text-centre">
-        <h6>BECAUSE IT STILL TAKES A VILLAGE:</h6>
+        <h6 style={{ marginTop: "1rem" }}>BECAUSE IT STILL TAKES A VILLAGE:</h6>
         <div className="card-title">What is Drea Day?</div>
-          <div className="card-body">
-          
-          <span className="text-muted">
+        <div className="card-body">
+          <span className="text-muted" style={{ marginBottom: "1rem" }}>
             On May 6th, Dreasjon "Sean" Reed was murdered by IMPD Officer
             De'Joure Mercer while he was streaming live via Facebook. Multiple
             eyewitnesses have come forward and have sworn under oath that the
@@ -34,52 +33,58 @@ const About = () => {
           </span>
         </div>
         <div>
-        <a href="https://www.gofundme.com/f/justice-for-dreasjon-sean-reed?utm_source=customer&utm_campaign=p_cp+share-sheet&utm_medium=copy_link_all" class="btn btn-info" role="button">DONATE NOW</a>
+          <a
+            style={{ marginBottom: "1rem" }}
+            href="https://www.gofundme.com/f/justice-for-dreasjon-sean-reed?utm_source=customer&utm_campaign=p_cp+share-sheet&utm_medium=copy_link_all"
+            class="btn btn-info"
+            role="button"
+          >
+            DONATE NOW{" "}
+          </a>
+        </div>
       </div>
-      </div>
-      <h3>Our Partners</h3>
-      <div className="container1">
-      <a href="https://edelenyc.com/">
-        <img src={edele} alt="Edele img" height={100} width={100} />
-      </a>
-      <a href="https://www.etsy.com/shop/AmericasStepdaddy">
-        <img src={img6278} alt="6278 img" height={100} width={100} />
-      </a>
-      <a href="https://www.uncommongoods.com/product/sweet-and-savory-hot-sauce-duo">
-        <img src={heat} alt="Heat img" height={100} width={100} />
-      </a>
-      <a href="https://www.marykay.com/ERivers">
-        <img src={mary} alt="Mary img" height={100} width={100} />
-      </a>
-      <a href="https://www.instagram.com/wowbeauty317/">
-        <img src={face} alt="Face img" height={100} width={100} />
-      </a>
+      <h3 style={{ marginTop: "1rem" }}>Our Partners</h3>
+      <div className="container1" style={{ marginTop: "2rem" }}>
+        <a href="https://edelenyc.com/">
+          <img src={edele} alt="Edele img" height={100} width={100} />
+        </a>
+        <a href="https://www.etsy.com/shop/AmericasStepdaddy">
+          <img src={img6278} alt="6278 img" height={100} width={100} />
+        </a>
+        <a href="https://www.uncommongoods.com/product/sweet-and-savory-hot-sauce-duo">
+          <img src={heat} alt="Heat img" height={100} width={100} />
+        </a>
+        <a href="https://www.marykay.com/ERivers">
+          <img src={mary} alt="Mary img" height={100} width={100} />
+        </a>
+        <a href="https://www.instagram.com/wowbeauty317/">
+          <img src={face} alt="Face img" height={100} width={100} />
+        </a>
       </div>
 
       <div className="container2">
-      <a href="https://www.instagram.com/culicovers/">
-        <img src={cc} alt="Cc img" height={100} width={100} />
-      </a>
-      <a href="https://www.amazon.com/dp/B01BA5NFAU/ref=cm_sw_r_cp_awdb_btf_t1_pStEFbEMCARYM">
-        <img src={smoosh} alt="Smoosh img" height={100} width={100} />
-      </a>
-      <a href="https://www.financialeducationservices.com/default.aspx?rid=ERivers3">
-        <img src={credit} alt="Credit img" height={100} width={100} />
-      </a>
-      <a href="https://livefreedombrand.com/">
-        <img src={crown} alt="Crown img" height={100} width={100} />
-      </a>
-      <a href="https://ameliasbread.com/">
-        <img src={amelia} alt="Amelia img" height={100} width={100} />
-      </a>
-      <a href="https://music.apple.com/us/album/too-late-single/1540121306?ls">
-        <img src={toolate} alt="Toolate img" height={100} width={100} />
-      </a>
-
+        <a href="https://www.instagram.com/culicovers/">
+          <img src={cc} alt="Cc img" height={100} width={100} />
+        </a>
+        <a href="https://www.amazon.com/dp/B01BA5NFAU/ref=cm_sw_r_cp_awdb_btf_t1_pStEFbEMCARYM">
+          <img src={smoosh} alt="Smoosh img" height={100} width={100} />
+        </a>
+        <a href="https://www.financialeducationservices.com/default.aspx?rid=ERivers3">
+          <img src={credit} alt="Credit img" height={100} width={100} />
+        </a>
+        <a href="https://livefreedombrand.com/">
+          <img src={crown} alt="Crown img" height={100} width={100} />
+        </a>
+        <a href="https://ameliasbread.com/">
+          <img src={amelia} alt="Amelia img" height={100} width={100} />
+        </a>
+        <a href="https://music.apple.com/us/album/too-late-single/1540121306?ls">
+          <img src={toolate} alt="Toolate img" height={100} width={100} />
+        </a>
       </div>
-      
     </div>
   );
 };
 
 export default About;
+
